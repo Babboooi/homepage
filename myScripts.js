8c1a61a91e3d76ab951efdff3e471ee756448b58
Babboooi <113873726+Babboooi@users.noreply.github.com>
2024-04-22 11:10:18 -0500

    ap22

---
+++ myScripts.js
@@ -0,0 +1,21 @@
+</body>
+
+<body onload="startTime()">
+
+<div id="clock"></div>
+//CLOCK SCRIPT//</body>
+function startTime() {
+ const today = new Date();
+ let h = today.getHours();
+ let m = today.getMinutes();
+ let s = today.getSeconds();
+ m = checkTime(m);
+ s = checkTime(s);
+ document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+ setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+ if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+ return i;
+}
