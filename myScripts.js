92b84feec47eee5af2593a557d9efe64da91837a
Babboooi <113873726+Babboooi@users.noreply.github.com>
2024-04-24 11:55:09 -0500

    apr24

--- myScripts.js
+++ myScripts.js
@@ -1,8 +1,3 @@
-</body>
-
-<body onload="startTime()">
-
-<div id="clock"></div>
 //CLOCK SCRIPT//</body>
 function startTime() {
  const today = new Date();
@@ -15,7 +10,45 @@ function startTime() {
  setTimeout(startTime, 1000);
 }
 
-function checkTime(i) {
+ function checkTime(i) {
  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
  return i;
 }
+
+//CALCULATOR (JAVASCRIPT)
+var showCalc = false;
+function showCalculator(){
+   if (showCalc == false){
+       document.getElementById("calc").style.opacity = 1;
+       showCalc = true;
+   }
+   else if (showCalc == true){
+       document.getElementById("calc").style.opacity = 0;
+       showCalc = false;
+   }
+}
+
+//POST-IT NOTE SAVE (JAVASCRIPT)
+if (localStorage["note"]) {
+    var note = localStorage["note"];
+    document.getElementById("note").value = note;
+ }
+  
+ function autoSave() {
+    var note = document.getElementById("note").value;
+    localStorage.setItem("note", note);
+ }
+ 
+ //POST-IT NOTE SHOW AND HIDE
+ var show = false;
+ function showNote(){
+    if (show == false){
+        document.getElementById("note").style.opacity = 1;
+        show = true;
+    }
+    else if (show == true){
+        document.getElementById("note").style.opacity = 0;
+        show = false;
+    }
+ }
+ 
